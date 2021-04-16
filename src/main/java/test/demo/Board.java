65b82c8dc57f655804131b4048afc2f2d9747406
SkSniperkS <SkSniperkS@users.noreply.github.com>
2021-04-16 17:44:45 +0200

    Basic turn logic

---
+++ src/main/java/test/demo/Board.java
@@ -0,0 +1,12 @@
+public class Board {
+    Card[][] deck = new Card[2][6];
+
+    public void assignCard(Card card, int[] index){
+        deck[index[0]][index[1]] = card;
+        card.onBoard = true;
+    }
+
+    public void removeCard(int[] index){
+        deck[index[0]][index[1]] = null;
+    }
+}
--- src/main/java/test/demo/Player.java
+++ src/main/java/test/demo/Player.java
@@ -1,8 +1,10 @@
 public class Player extends BaseCard{
     int maxDeployPoints;
+    Card[] availableCards;
 
-    public Player(int _maxHP, int _maxDeployPoints, String _name, String _image){
+    public Player(int _maxHP, int _maxDeployPoints, String _name, String _image, Card[] _availableCards){
         super(_maxHP, _name, _image);
         maxDeployPoints = _maxDeployPoints;
+        availableCards = _availableCards;
     }
 }
