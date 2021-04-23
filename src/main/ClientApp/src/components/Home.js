06ab07456ff5ad927be474187441bfec6a459003
Denis265 <68737422+Denis265@users.noreply.github.com>
2021-04-23 16:30:57 +0200

    Update Home.js

--- src/main/ClientApp/src/components/Home.js
+++ src/main/ClientApp/src/components/Home.js
@@ -1,18 +1,32 @@
+import { event } from 'jquery';
 import React, { Component } from 'react';
 
 export class Home extends Component {
   static displayName = Home.name;
 
+  constructor(props){
+    super(props);
+    this.state = {username : ''}
+  }
+
+  buttonOkClick = (event) => {
+    let name = this.state.username;
+    alert('Ok, your username is:' + name);
+  }
+
+  inputTextChange = (event) => {
+    let name = event.target.value;
+    this.setState({username: name})
+  }
+
   render () {
     return (
       <div>
-        <h1>Hello, All!</h1>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+       <h1>Hello</h1>
+       <input type="text" onChange={this.inputTextChange}/>
+       <br /><br />
+
+       <button className="btn btn-primary" onClick={this.buttonOkClick}>Ok</button>
       </div>
     );
   }
